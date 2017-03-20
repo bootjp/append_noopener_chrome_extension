7a98c621dfe3702afe69b4c64031a660c3808706
bootjp <oh@bootjp.me>
2017-03-20 14:33:39 +0900

    modify / manifest.json, add.js -> noopenerを追加するのを高速化するため、content_scriptの呼び出しタイミングをdocument_start時に変更し、DOMContentLoadedのイベントを待機するように変更

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   Array.prototype.slice.call(document.getElementsByTagName('a')).forEach((element) => {
     if (element.target === '_blank' && element.rel.indexOf('noopener') === -1) {
