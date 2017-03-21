97c8aad465b8aec88aeb061eec1f1858bd3b48e0
bootjp <oh@bootjp.me>
2017-03-21 14:45:11 +0900

    forEach分の中の条件式が冗長だったため、filterしてから行うように変更

--- add.js
+++ add.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
-  Array.prototype.slice.call(document.getElementsByTagName('a')).forEach((element) => {
-    if (element.target === '_blank' && element.rel.indexOf('noopener') === -1) {
-      element.rel += ' noopener';
-    }
+  Array.prototype.slice.call(document.getElementsByTagName('a')).filter((element) => (
+    element.target === '_blank' && element.rel.indexOf('noopener') === -1
+  )).forEach((element) => {
+    element.rel += ' noopener';
   });
 });
